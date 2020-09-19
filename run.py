4e55f8dca8491ee918fcec1f4479d7f295161d50
mmt <pycdremail@gmail.com>
2020-09-19 20:19:58 +0430

    Debuggin and Adding comments and helps

--- run.py
+++ run.py
@@ -6,52 +6,66 @@ from sys import stdout
 from json import load
 from export import make_img, make_txt
 
-config = load(open("config.json",'r'))
+config = load(open("config.json",'r')) # load from config.json
 
 def main():
+	# get arguments
 	parser = AP()
-	parser.add_argument("--cfrom", help="input text for convert mode", required=False)
-	parser.add_argument("--cto", help="output image for convert mode", required=False)
-	parser.add_argument("--rcfrom", help="input text for reconvert mode", required=False)
-	parser.add_argument("--rcto", help="output image for reconvert mode", required=False)
+	parser.add_argument("-i", "--input", help="input image/txt", required=True)
+	parser.add_argument("-o", "--output", help="output image/txt", required=True)
 	args = parser.parse_args()
-	print("~> Pyshred pro ver2.0")
-	if (bool(args.rcfrom)!=bool(args.rcto)) or (bool(args.cfrom)!=bool(args.cto)):
-		print("please give input and output together!")
+	# check arguments
+	if (not exists(args.input)): # input does not exists
+		print("please give an existing path to input!")
 		return
-	if args.rcfrom and args.rcto:
-		if (not exists(args.rcfrom)):
-			print("please give an existing path to input!")
-			return
-		if args.rcfrom.endswith(".txt"):
-			print("> read text file...")
-			txt = open(args.rcfrom, 'r').read()
-		else:
-			print("> start convert mode...")
-			txt = make_txt(args.rcfrom)
+	if args.input.endswith(".txt") and args.output.endswith(".txt"): # we can't export txt to txt!
+		print("text to text?? really?? :/")
+		return
+	# set mode
+	if args.output.endswith(".txt"):
+		# convert mode: image to txt
+		print("> start convert mode...")
+		txt = make_txt(
+			args.input,
+			config["convert"]["output"]["width"],
+			config["convert"]["output"]["height"]
+		)
+		print("> output ...")
+		with open(args.output, 'w') as f:
+			f.write(txt)
+			f.close()
+		print("== done! ==")
+	elif args.input.endswith(".txt"):
+		# reconvert mode: txt to image
 		print("> start reconvert mode...")
+		print("> read text file...")
+		txt = open(args.input, 'r').read()
+		print("> making array...")
 		arr = make_img(
 			txt,
 			config["reconvert"]["output"]["width"],
 			config["reconvert"]["output"]["height"]
 		)
 		print("> output...")
-		imwrite(args.rcto, arr)
+		imwrite(args.output, arr)
 		print("== done! ==")
-	elif args.cfrom and args.cto:
-		if (not exists(args.cfrom)):
-			print("please give an existing path to input!")
-			return
+	else:
+		# dualconvert mode: image to image !
+		print("> start dual convert mode...")
 		print("> start convert mode...")
 		txt = make_txt(
-			args.cfrom,
+			args.input,
 			config["convert"]["output"]["width"],
 			config["convert"]["output"]["height"]
 		)
-		print("> output ...")
-		with open(args.cto, 'w') as f:
-			f.write(txt)
-			f.close()
+		print("> start convert mode...")
+		arr = make_img(
+			txt,
+			config["reconvert"]["output"]["width"],
+			config["reconvert"]["output"]["height"]
+		)
+		print("> output...")
+		imwrite(args.output, arr)
 		print("== done! ==")
 
 if __name__=='__main__':
