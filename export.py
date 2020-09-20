b2aa547da76050e997631cb451f4d1f1b72c4aee
mmt <pycdremail@gmail.com>
2020-09-20 10:33:09 +0430

    adding logs in files

--- export.py
+++ export.py
@@ -2,16 +2,21 @@ from cv2 import imread, imwrite, resize
 from numpy import array,zeros,uint8
 from argparse import ArgumentParser as AP
 from tools import char, sub
+from log import log
+
 """
 here is 2 functions that with them, you will be able to convert text to image or image to text, like its usage in main file(run.py)
 DON'T EDIT IF YOU ARE GOING TO TEST YOURSELF/ITSELF, OR YOU DIDN'T UNDERSTAND IT CAREFULLY, PLEASE!
 """
+
 def make_img(txt, width="input", height="input"): # convert txt to image array
 	"""convert <string> to image <array>.
 + notice that width and height will increase 5 times.
 + for example if width=30 and height=60, output will be 150x300 (30*5=150, 60*5=300).
 + why? because any word from <string> uses 5x5 area from output <array>.
 DON'T EDIT IF YOU ARE GOING TO TEST YOURSELF/ITSELF, OR YOU DIDN'T UNDERSTAND IT CAREFULLY, PLEASE!"""
+	log("start reconvert mode", "export.py", "make_img()")
+	log("settings", "export.py", "make_img()")
 	print("# get data...")
 	_w = txt.find("\n")
 	_h = len(txt.split("\n"))
@@ -23,10 +28,12 @@ DON'T EDIT IF YOU ARE GOING TO TEST YOURSELF/ITSELF, OR YOU DIDN'T UNDERSTAND IT
 		height = _h
 	else:
 		height = int(height)
-	print("# make array...")
+	log("making array", "export.py", "make_img()")
+	print("# making array...")
 	arr = zeros((_h*5, _w*5, 3),uint8)
 	pos1 = [0,0]
 	d = {"X":(255,255,255),"-":(0,0,0)}
+	log("start reconvert loop", "export.py", "make_img()")
 	print("# start loop...")
 	for x in txt.split("\n"):
 		pos1[0]=0
@@ -41,18 +48,24 @@ DON'T EDIT IF YOU ARE GOING TO TEST YOURSELF/ITSELF, OR YOU DIDN'T UNDERSTAND IT
 				pos2[1]+=1
 			pos1[0]+=5
 		pos1[1]+=5
+	log("loop finished", "export.py", "make_img()")
 	print("# loop finished")
+	log("resizing", "export.py", "make_img()")
 	print("# reiszing...")
 	arr = resize(arr, (width*5, height*5))
+	log("reonvert mode has done", "export.py", "make_img()")
 	print("# reconvert mode finished")
 	return arr
 
 def make_txt(path, width="input", height="input"): # convert image to txt
 	"""read [path] and get its <array>, then convert <array> to <string>
 DON'T EDIT IF YOU ARE GOING TO TEST YOURSELF/ITSELF, OR YOU DIDN'T UNDERSTAND IT CAREFULLY, PLEASE!"""
+	log("start convert mode", "export.py", "make_txt()")
+	log("load image", "export.py", "make_txt()")
 	print("# load image...")
 	img = imread(path, 0)
 	print("# get data...")
+	log("settings", "export.py", "make_txt()")
 	if width=="input":
 		width=len(img)
 	else:
@@ -61,13 +74,17 @@ DON'T EDIT IF YOU ARE GOING TO TEST YOURSELF/ITSELF, OR YOU DIDN'T UNDERSTAND IT
 		height=len(img[0])
 	else:
 		height=int(height)
+	log("resizing", "export.py", "make_txt()")
 	print("# resizing...")
 	img = resize(img,(height, width))
 	text = ""
+	log("start convert loop", "export.py", "make_txt()")
 	print("# start loop...")
 	for i in img:
 		for j in i:
 			text += sub(j)
 		text += '\n'
+	log("loop finished", "export.py", "make_txt()")
+	log("convert mode has  done", "export.py", "make_txt()")
 	print("# convert mode finished")
 	return text
--- run.py
+++ run.py
@@ -5,68 +5,94 @@ from os.path import exists
 from sys import stdout
 from json import load
 from export import make_img, make_txt
+from log import log
 
-config = load(open("config.json",'r')) # load from config.json
 
 def main():
 	# get arguments
+	log("start <main> function", "run.py", "main()")
 	parser = AP()
 	parser.add_argument("-i", "--input", help="input image/txt", required=True)
 	parser.add_argument("-o", "--output", help="output image/txt", required=True)
 	args = parser.parse_args()
+	log("getting arguments", "run.py", "main()")
 	# check arguments
 	if (not exists(args.input)): # input does not exists
+		log("input agrument does not exist", "run.py", "main()", "ERROR")
 		print("please give an existing path to input!")
 		return
 	if args.input.endswith(".txt") and args.output.endswith(".txt"): # we can't export txt to txt!
+		log("invalid mode", "run.py", "main()", "ERROR")
 		print("text to text?? really?? :/")
 		return
+	
 	# set mode
+	log("selecting mode", "run.py", "main()")
 	if args.output.endswith(".txt"):
 		# convert mode: image to txt
+		log("set on convert mode", "run.py", "main()")
 		print("> start convert mode...")
+		log("convert text to array", "run.py", "main()", "convert-mode")
 		txt = make_txt(
 			args.input,
 			config["convert"]["output"]["width"],
 			config["convert"]["output"]["height"]
 		)
+		log("output", "run.py", "main()", "convert-mode")
 		print("> output ...")
 		with open(args.output, 'w') as f:
 			f.write(txt)
 			f.close()
+		log("done", "run.py", "main()", "convert-mode")
 		print("== done! ==")
 	elif args.input.endswith(".txt"):
 		# reconvert mode: txt to image
+		log("set on reconvert mode", "run.py", "main()")
 		print("> start reconvert mode...")
+		log("get input text", "run.py", "main()", "reconvert-mode")
 		print("> read text file...")
 		txt = open(args.input, 'r').read()
+		log("convert text to array", "run.py", "main()", "reconvert-mode")
 		print("> making array...")
 		arr = make_img(
 			txt,
 			config["reconvert"]["output"]["width"],
 			config["reconvert"]["output"]["height"]
 		)
+		log("output array", "run.py", "main()", "reconvert-mode")
 		print("> output...")
 		imwrite(args.output, arr)
+		log("done", "run.py", "main()", "reconvert-mode")
 		print("== done! ==")
 	else:
 		# dualconvert mode: image to image !
-		print("> start dual convert mode...")
+		log("set on dualconvert mode", "run.py", "main()")
+		print("> start dualconvert mode...")
+		log("start convert mode", "run.py", "main()", "dualconvert-mode")
 		print("> start convert mode...")
 		txt = make_txt(
 			args.input,
 			config["convert"]["output"]["width"],
 			config["convert"]["output"]["height"]
 		)
-		print("> start convert mode...")
+		log("convert mode has done", "run.py", "main()", "dualconvert-mode")
+		log("start reconvert mode", "run.py", "main()", "dualconvert-mode")
+		print("> start reconvert mode...")
 		arr = make_img(
 			txt,
 			config["reconvert"]["output"]["width"],
 			config["reconvert"]["output"]["height"]
 		)
+		log("reconvert mode has done", "run.py", "main()", "dualconvert-mode")
+		log("output", "run.py", "main()", "dualconvert-mode")
 		print("> output...")
 		imwrite(args.output, arr)
+		log("done", "run.py", "main()", "dualconvert-mode")
 		print("== done! ==")
+	log("END")
 
 if __name__=='__main__':
+	log("START")
+	config = load(open("config.json",'r')) # load from config.json
+	log("config has loaded", "run.py", "__main__")
 	main()
